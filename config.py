460e1dafe31fd6ca7a01ac4114f373e99c3b860f
Yaroslav Bairamgalin <ybairamgalin@gmail.com>
2022-03-19 17:06:55 +0300

    UPD: removed unneccessary config info

--- config.py
+++ config.py
@@ -2,9 +2,3 @@
 
 """telegram bot token"""
 TOKEN = "5176789554:AAHoDD4EG770PlCXvX9z1UyLkTiTDYmhmqM"
-
-"""database info"""
-DATABASE = "olx-parser"
-USERNAME = "admin"
-PASSWORD = "admin"
-HOST = "localhost"
